afb3edc972ce72d8a8ff0126de3d69f7d5968279
betulyazici <betul.yzc@hotmail.com>
2022-10-24 20:30:38 +0300

    actuator dependency added.

--- warehouse-service/src/main/java/com/project/warehouseservice/WarehouseServiceApplication.java
+++ warehouse-service/src/main/java/com/project/warehouseservice/WarehouseServiceApplication.java
@@ -2,15 +2,8 @@ package com.project.warehouseservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
-@EnableSwagger2
 public class WarehouseServiceApplication {
 
 	public static void main(String[] args) {
@@ -18,10 +11,5 @@ public class WarehouseServiceApplication {
 	}
 
 
-	@Bean
-	public Docket apiDoc() {
-		return new Docket(DocumentationType.SWAGGER_2).select().apis(RequestHandlerSelectors.any())
-				.paths(PathSelectors.any()).build();
-	}
 
 }
